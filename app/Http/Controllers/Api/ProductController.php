dcbc203cc097854c50e015a337d48922e8f31682
MirzaaAF <mirzaaulia1865@gmail.com>
2022-05-13 01:43:42 +0700

    category

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -18,6 +18,7 @@ class ProductController extends Controller
      */
     public function index()
     {
+
         $product = Product::all();
 
         return response()->json([
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,11 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class product extends Model
 {
     use HasFactory;
-
+    public function Readproduk()
+    {
+        $product = DB::table('products')->get();
+        return $product;
+    }
     protected $tabel = 'products';
     protected $fillable = [
         'id',
@@ -28,5 +33,4 @@ class product extends Model
     {
         return $this->hasOne(Category::class, "id", "subcategory_id");
     }
-
 }
