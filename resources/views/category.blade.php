ec0457eaf0b8a56ecef37c9a3c1e181481bf4f2c
MirzaaAF <mirzaaulia1865@gmail.com>
2022-06-01 08:54:32 +0700

    FE : Resolve Conflict

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -166,6 +166,10 @@ session_start();
                                                 <td> {{ $category->id }} </td>
                                                 <td> {{ $category->name }} </td>
                                                 <td> {{ $category->desc }} </td>
+                                                <td align="center">
+                                                    <button style="padding:5px" type="button" class="btn btn-primary align:center" data-toggle="modal" data-target="#ModalEdit"><i class="fa fa-pencil-square fa-2x" aria-hidden="true"></i> </button>
+                                                    <button style="padding:5px" type="button" class="btn btn-danger" data-toggle="modal" data-target="#ModalDelete"><i class="fa fa-trash fa-2x" aria-hidden="true"></i></i> </button>
+                                                </td>
                                             </tr>
                                             @endforeach
                                         </tbody>
